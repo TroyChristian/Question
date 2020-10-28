594d05df52652dfad905af62ef913367b617fccd
TroyChristian <apostasy8@gmail.com>
2020-10-28 01:17:13 -0400

    add register and login routes

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, g, request 
+from database import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_db(error):
+	if hasattr(g, 'sqlite_db' ):
+		g.sqlite_db.close() 
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+	if request.method == 'POST':
+		db = get_db()
+		hashed_password = generate_password_hash(request.form['password'], method='sha256')
+		db.execute('insert into users (name, password, expert, admin) values (?,?,?,?)', [request.form['name'], hashed_password, '0','0'])
+		db.commit() 
+		print("succesfully added user")
+		return 'User Created'
+	return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+	if request.method == "POST":
+		name = request.form['name']
+		password = request.form['password']
+		db = get_db()
+		user_cursor = db.execute("SELECT id, name, password FROM users WHERE name = ?", [name])
+		user_result = user_cursor.fetchone() 
+		if check_password_hash(user_result["password"], password):
+			return "<h6> The password is correct </h6>"
+		else:
+			return "<h6> The password is incorrect </h6>"
+	return render_template('login.html')
 
 @app.route('/question')
 def question():
